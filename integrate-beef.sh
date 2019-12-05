c5edd442dafffab8e7f669eeb811846f42178f18
t0rben <45014600+rootorben@users.noreply.github.com>
2019-12-06 00:30:27 +0100

    Create integrate-beef.sh

---
+++ integrate-beef.sh
@@ -0,0 +1,3 @@
+cd && git clone https://github.com/xntrik/beefmetasploitplugin;
+sudo cp -r beefmetasploitplugin/lib /usr/share/metasploit-framework/lib && sudo cp -r beefmetasploitplugin/plugins /usr/share/metasploit-framework/plugins;
+sudo nano /usr/share/beef-xss/config.yaml
