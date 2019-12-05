fb5e98f24ffa8af01c16965ad833058f20b6d4ed
t0rben <45014600+rootorben@users.noreply.github.com>
2019-12-06 00:38:22 +0100

    Update integrate-beef.sh

--- integrate-beef.sh
+++ integrate-beef.sh
@@ -1,3 +1,9 @@
 cd && git clone https://github.com/xntrik/beefmetasploitplugin;
 sudo cp -r beefmetasploitplugin/lib /usr/share/metasploit-framework/lib && sudo cp -r beefmetasploitplugin/plugins /usr/share/metasploit-framework/plugins;
-sudo nano /usr/share/beef-xss/config.yaml
+cd /opt;
+sudo git clone --recursive https://github.com/rsmudge/cortana-scripts;
+sudo git clone --recursive https://github.com/HarmJ0y/cortana;
+sudo chmod 755 /root && cd /root;
+sudo ln -s /opt/cortana-scripts;
+sudo ln -s /opt/cortana;
+cd && sudo nano /usr/share/beef-xss/config.yaml
